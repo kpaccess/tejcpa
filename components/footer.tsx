7d69797eca288ee35efeec06a6d0240021101de5
krishna pradhan <kpaccess@gmail.com>
2024-05-27 13:09:14 -0400

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,6 @@
-function BriefcaseIcon(props) {
+function BriefcaseIcon() {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -18,29 +17,28 @@ function BriefcaseIcon(props) {
   );
 }
 
-function CheckIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M20 6 9 17l-5-5" />
-    </svg>
-  );
-}
+// function CheckIcon(props) {
+//   return (
+//     <svg
+//       {...props}
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     >
+//       <path d="M20 6 9 17l-5-5" />
+//     </svg>
+//   );
+// }
 
-function MailIcon(props) {
+function MailIcon() {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -57,10 +55,9 @@ function MailIcon(props) {
   );
 }
 
-function PhoneIcon(props) {
+function PhoneIcon() {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
